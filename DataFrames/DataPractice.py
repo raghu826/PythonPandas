53d4c2c737abe74f30748809d2ec28f24e4ef007
Your Name <yraghu13@gmail.com>
2020-10-30 02:35:05 +0100

    practiced iris dataset for visualisation

--- DataFrames/DataPractice.py
+++ DataFrames/DataPractice.py
@@ -1,12 +1,24 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import pylab as pl
+import numpy as np
+
 
 a = pd.read_csv(r'C:\Users\telelink\Downloads\100SalesRecords.csv')
-print(a)   # to print total data frame
+# print(a)   # to print total data frame
+# print(a.head())
 
-print(a['Total Profit'].max()) # to get maximum profit
+columns = a[['Region','Unit Cost','Total Cost', 'Total Profit']]
 
-print(a.columns) # to get list of columns
+print(columns.head())
 
-print(a['Region'][a['Total Profit']==a['Total Profit'].max()]) # to get particular
+# print(a['Total Profit'].max()) # to get maximum profit
 
-print(a[:]['Region'])  # to get a particular column
+# print(a.columns) # to get list of columns
+
+# print(a['Region'][a['Total Profit']==a['Total Profit'].max()]) # to get particular
+
+# print(a[:]['Region'])  # to get a particular column
+
+columns.hist()
+plt.show()
---
+++ DataFrames/Iris.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import pylab as pl
+import numpy as np
+import seaborn as sns
+
+irisData = pd.read_csv(r'C:\Users\telelink\Contacts\Desktop\irisDataSet.csv')
+print(irisData.columns)
+
+# To change Column names use rename
+newData = irisData.rename(columns={"Sepal.Length": "SepalLength","Petal.Length": "PetalLength",
+                      "Sepal.Width": "SepalWidth","Petal.Width": "PetalWidth"})
+print(newData.columns)
+
+# To plot PetalLength n SepalLength
+plt.scatter(newData.PetalLength, newData.SepalLength, color = 'red')
+plt.xlabel("PetalLength")
+plt.ylabel("SepalLength")
+
+# To plot indicating species with different colors using seaborn
+sns.set_style('whitegrid')
+sns.FacetGrid(newData, hue = "Species", size =4 ).map(plt.scatter,"PetalLength","SepalLength").add_legend()
+plt.xlabel("PetalLength")
+plt.ylabel("SepalLength")
+plt.show()
+
+
+
+
+
